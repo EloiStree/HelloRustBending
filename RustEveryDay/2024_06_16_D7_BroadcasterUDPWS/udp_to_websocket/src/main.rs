4b3c92e767f2dbc986ebcfd0dbea42781473922c
Eloi Stree <eloistree@gmail.com>
2024-06-16 16:51:45 +0200

    save D7

--- RustEveryDay/2024_06_16_D7_BroadcasterUDPWS/udp_to_websocket/src/main.rs
+++ RustEveryDay/2024_06_16_D7_BroadcasterUDPWS/udp_to_websocket/src/main.rs
@@ -16,17 +16,29 @@ async fn main() {
     // Shared list of WebSocket clients
     let clients: Arc<Mutex<Vec<UnboundedSender<Message>>>> = Arc::new(Mutex::new(Vec::new()));
 
-    let udp_thread_clients: Arc<Mutex<Vec<UnboundedSender<Message>>>> = clients.clone();
-    let udp_thread: thread::JoinHandle<_> = thread::spawn(move || {
-        let udp_ipv4_and_port: String = "127.0.0.1:1236".to_string();
+    // Clone for UDP thread
+    let udp_thread_clients = clients.clone();
+
+    // Spawn UDP server thread
+    let udp_thread = thread::spawn(move || {
+        let udp_ipv4_and_port = "127.0.0.1:1236".to_string();
         println!("UDP Server is running at {}", udp_ipv4_and_port);
-        let socket: UdpSocket = UdpSocket::bind(udp_ipv4_and_port).expect("Failed to bind socket");
+        let socket = UdpSocket::bind(udp_ipv4_and_port).expect("Failed to bind socket");
         let mut buffer = [0; 1024];
         loop {
             let (size, _) = socket.recv_from(&mut buffer).expect("Failed to receive data");
             let data = &buffer[..size];
-            println!("Received: {:?} from {:?}", data, socket.local_addr().unwrap());
-
+            if data.len() <= 50 {
+                println!("Received: {:?} from {:?}", data, socket.local_addr().unwrap());
+            }
+            else {
+                println!("Received: {:?} from {:?}", &data[0..50], socket.local_addr().unwrap());
+            }
+            if data.len() == 4 {
+                let num: i32 = i32::from_le_bytes([data[0], data[1], data[2], data[3]]);
+                println!("Received as i32: {}", num);
+            }
+            // Broadcast to all WebSocket clients
             let clients = udp_thread_clients.lock().unwrap();
             for client in clients.iter() {
                 if let Err(e) = client.send(Message::Binary(data.to_vec())) {
@@ -36,12 +48,27 @@ async fn main() {
         }
     });
 
+    // Clone for TCP server thread
     let tcp_server_clients = clients.clone();
+
+    // Spawn TCP server thread
     let tcp_server_thread = thread::spawn(move || {
         let rt = Runtime::new().unwrap();
         rt.block_on(run_server(tcp_server_clients));
     });
 
+    // Main thread loop to periodically check and clean up disconnected clients
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(3)).await; // Adjust as needed
+
+        // Clean up disconnected clients
+        {
+            let mut clients = clients.lock().unwrap();
+            clients.retain(|client| !client.is_closed());
+        }
+    }
+
+    // Join threads
     udp_thread.join().unwrap();
     tcp_server_thread.join().unwrap();
 }
